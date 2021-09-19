958533889ce93ccbebd556ca1e4c113ebe3c21a8
YoelEigner <35924447+YoelEigner@users.noreply.github.com>
2021-09-19 12:22:51 +0300

    test

--- movies/BLs/search.js
+++ movies/BLs/search.js
@@ -1,6 +1,6 @@
 const allMvs = require("./moviesBL");
 
-let test = 0 
+let test
 const movie = async (text) => {
   let allMovies = await allMvs.movies();
   let movie = allMovies.filter((x) => x.name.includes(text));
